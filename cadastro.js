6fff548f6feacef2985da17a9500e9f6c0e994db
Rafael Amaral <rafaelosama80@gmail.com>
2023-02-03 11:02:38 -0300

    Arrumando bug do sistema

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+
+function singUp(email){
+    return email;
+}
+
+//pegando o email e cadastrando na plataforma.
+
+function pegaEmail(email){
+    return 'Bem vindo ao sistema';
+}
+
+//Adicionando função de mensagem
